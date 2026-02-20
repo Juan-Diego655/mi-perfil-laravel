41830dbcbf58a73539ad27a8f1f22f3190e140fa
Juan Diego Niño Solano <jnino825@unab.edu.co>
2026-02-19 21:26:47 -0500

    Vista perfil.blade.php creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,8 @@
+<h1>Mi Perfil</h1>
+<p>Nombre: Tu Nombre</p>
+<p>Carrera: Ingeniería de Sistemas</p>
+<p>Universidad: UNAB</p>
+
+<a href="/perfil/intereses">Intereses</a>
+<a href="/perfil/habilidades">Habilidades</a>
+<a href="/perfil/metas">Metas</a>
